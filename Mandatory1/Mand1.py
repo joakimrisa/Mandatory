1417efb4e4d7e80c4e0caae381c99a57ab97d362
Joakim Risa Sloreby <joakrs14@student.uia.no>
2018-01-25 17:14:41 +0100

    Updated version 1.0

--- Mandatory1/Mand1.py
+++ Mandatory1/Mand1.py
@@ -5,6 +5,12 @@ import math
 
 MAXPHEROMONES = 100000
 MINPHEROMONES = 1
+nodes = dict()
+edges = dict()
+MAXCOST = 0
+bestScore = 0
+bestSolution = []
+currentScore = 0
 
 
 class Node:
@@ -15,17 +21,23 @@ class Node:
     def rouletteWheelSimple(self):
         return random.sample(self.edges, 1)[0]
 
-    def rouletteWheel(self, visitedEdges, startNode):
+    def rouletteWheel(self, visitedEdges, startNode, endNode, numCities):
         visitedNodes = [oneEdge.toNode for oneEdge in visitedEdges]
-        viableEdges = [oneEdge for oneEdge in self.edges if
-                       not oneEdge.toNode in visitedNodes and oneEdge.toNode != startNode]
-        if not viableEdges:
-            viableEdges = [oneEdge for oneEdge in self.edges if not oneEdge.toNode in visitedNodes]
+        # print(visitedNodes.__len__())
+        if numCities <= visitedNodes.__len__():
+            for edge in self.edges:
+                if edge.toNode == endNode:
+                    viableEdges = [edge]
+                    break
+        else:
+            viableEdges = [oneEdge for oneEdge in self.edges if
+                           not oneEdge.toNode in visitedNodes and oneEdge.toNode != startNode and oneEdge.toNode != endNode]
 
         allPheromones = sum([oneEdge.pheromones for oneEdge in viableEdges])
         num = random.uniform(0, allPheromones)
         s = 0
         i = 0
+
         selectedEdge = viableEdges[i]
         while (s <= num):
             selectedEdge = viableEdges[i]
@@ -51,90 +63,12 @@ class Edge:
             self.pheromones = MINPHEROMONES
 
     def __repr__(self):
-        return self.fromNode + "--(" + str(self.cost) + ")--" + self.toNode
+        return self.fromNode.name + "--(" + str(self.cost) + ")--" + self.toNode.name
 
-
-'''
-a = Node("A")
-b = Node("B")
-c = Node("C")
-d = Node("D")
-e = Node("E")
-
-nodes = [a, b, c, d, e]
-edges = [
-    Edge(a, b, 100),
-    Edge(a, c, 175),
-    Edge(a, d, 100),
-    Edge(a, e, 75),
-    Edge(b, c, 50),
-    Edge(b, d, 75),
-    Edge(b, e, 125),
-    Edge(c, d, 100),
-    Edge(c, e, 125),
-    Edge(d, e, 75)]
-'''
-
-
-def loader(startPoint, endPoint, nCities, country="no"):
-    #Make so it asks for country file
-    nodes = dict()
-    edges = dict()
-    with codecs.open("worldcitiespop.csv", "r", encoding="utf-8", errors="ignore") as f:
-        allCities = csv.reader(f, delimiter=',', quotechar='|')
-        possibrahCities = dict()
-        for line in allCities:
-            if line[0] == country and line[1].__len__() > 1:
-                la = int(float(line[5])*10)
-                lo = int(float(line[6])*10)
-                hashval = la*lo
-                if not hashval in possibrahCities:
-                    #possibrahCities[hashval] = []
-                    possibrahCities[hashval] = line
-                elif possibrahCities[hashval][4] < line[4]:
-                    possibrahCities[hashval] = line
-
-                # cities = f.readlines()
-                # print(cities)
-        #print(possibrahCities)
-        print(possibrahCities.__len__())
-    for departure in possibrahCities:
-        la1 = possibrahCities[departure][5] #x
-        lo1 = possibrahCities[departure][6] #y
-        nodes[possibrahCities[departure][1]] = Node(possibrahCities[departure][1].upper())
-        for destination in possibrahCities:
-            if departure != destination:
-                la2 = possibrahCities[destination][5] #x1
-                lo2 = possibrahCities[destination][6] #y1
-                xDot = abs(float(la2)-float(la1))
-                yDot = abs(float(lo2)-float(lo1))
-                distance = math.sqrt(xDot**2+yDot**2)
-                #print(distance)
-                ##
-                if not possibrahCities[departure][1] in edges:
-                    edges[possibrahCities[departure][1]] = []
-                edges[possibrahCities[departure][1]].append(Edge(possibrahCities[departure][1], possibrahCities[destination][1], distance))
-#    print(edges)
-
-    #Write to file
-loader("st", "ab", 2)
-'''
-
-# Make symetrical
-for oneEdge in edges[:]:
-    edges.append(Edge(oneEdge.toNode, oneEdge.fromNode, oneEdge.cost))
-
-# Assign to nodes
-for oneEdge in edges:
-    for oneNode in nodes:
-        if (oneEdge.fromNode == oneNode):
-            oneNode.edges.append(oneEdge)
-
-'''
-'''
 def checkAllNodesPresent(edges):
     visitedNodes = [edge.toNode for edge in edges]
-    return set(nodes).issubset(visitedNodes)
+
+    return set(nodes.values()).issubset(visitedNodes)
 
 
 class Greedy:
@@ -147,9 +81,9 @@ class Greedy:
         currentEdge = None
         while (not checkAllNodesPresent(self.visitedEdges)):
             possibleEdges = [(edge.cost, edge) for edge in currentNode.edges if edge.toNode not in self.visitedNodes]
-            #print(possibleEdges)
-            #possibleEdges.sort()
-            possibleEdges.sort(key=lambda edge:edge[0])
+            # print(possibleEdges)
+            # possibleEdges.sort()
+            possibleEdges.sort(key=lambda edge: edge[0])
             # import pdb;pdb.set_trace()
             currentEdge = possibleEdges[0][1]
             currentNode = currentEdge.toNode
@@ -158,6 +92,88 @@ class Greedy:
             print(currentNode, currentEdge)
 
 
+def loader(country="no", limit=100):
+    # Make so it asks for country file
+    global edges
+    global nodes
+    with codecs.open("worldcitiespop.csv", "r", encoding="utf-8", errors="ignore") as f:
+        allCities = csv.reader(f, delimiter=',', quotechar='|')
+        possibrahCities = dict()
+        for line in allCities:
+            if possibrahCities.__len__() == limit:
+                break
+            if line[0] == country and line[1].__len__() > 2:
+                line[1] = line[1].replace(" ", "")
+                print("Starter på nytt dd")
+                # nodes[line[1]] = Node(line[1].upper())
+                la = int(float(line[5]) * 100)
+                lo = int(float(line[6]) * 100)
+                hashval = la * lo
+                if not hashval in possibrahCities:
+                    if not line[1] in nodes:
+                        print(len(possibrahCities))
+                        print(len(nodes))
+                        possibrahCities[hashval] = line
+                        nodes[line[1]] = Node(line[1].upper())
+                        print("1")
+                        print(len(possibrahCities))
+                        print(len(nodes))
+
+                elif possibrahCities[hashval][4] < line[4]:
+                    del nodes[possibrahCities[hashval][1]]
+                    possibrahCities[hashval] = line
+                    nodes[line[1]] = Node(line[1].upper())
+                    print("2")
+
+                elif possibrahCities[hashval][1] != line[1]:
+                    print("3")
+                    print(len(possibrahCities))
+                    print(len(nodes))
+                    del nodes[possibrahCities[hashval][1]]
+                    possibrahCities[hashval] = line
+                    print(len(line[1]))
+                    if line[1] == 'akarvik':
+                        print("EG E NÅ UTEN SPACE")
+                    print(line[1])
+                    nodes[possibrahCities[hashval][1]] = Node(line[1].upper())
+
+                #kebabfix, må endres om goody sier at vi må ha alle byer med ;)
+
+    for i in possibrahCities:
+        if not possibrahCities[i][1] in nodes:
+            print("AVIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIK")
+
+    print(possibrahCities)
+    print(nodes)
+
+
+    for departure in possibrahCities:
+        la1 = possibrahCities[departure][5]  # x
+        lo1 = possibrahCities[departure][6]  # y
+        for destination in possibrahCities:
+            if departure != destination:
+                la2 = possibrahCities[destination][5]  # x1
+                lo2 = possibrahCities[destination][6]  # y1
+                xDot = abs(float(la2) - float(la1))
+                yDot = abs(float(lo2) - float(lo1))
+                distance = math.sqrt(xDot ** 2 + yDot ** 2)
+                # print(distance)
+                ##
+                if not possibrahCities[departure][1] in edges:
+                    edges[possibrahCities[departure][1]] = []
+                edges[possibrahCities[departure][1]].append(
+                    Edge(nodes[possibrahCities[departure][1]], nodes[possibrahCities[destination][1]], distance))
+
+        nodes[possibrahCities[departure][1]].edges = edges[possibrahCities[departure][1]]
+        edges[possibrahCities[departure][1]] = []
+        print(nodes[possibrahCities[departure][1]].edges.__len__())
+    # print(nodes['aabjorgan'].edges)
+    print(nodes.__len__())
+    # print(nodes['aabjorgan'].edges.__len__())
+    # Write to file
+
+
+'''
 print("Greedy")
 g = Greedy()
 g.walk(a)
@@ -165,34 +181,47 @@ print("Cost:", sum([e.cost for e in g.visitedEdges]))
 
 
 # Cost function
+'''
+
+
 def getSum(edges):
     return sum(e.cost for e in edges)
 
 
-MAXCOST = getSum(edges)
-bestScore = 0
-bestSolution = []
-
-'''
-'''
 class ANT:
     def __init__(self):
         self.visitedEdges = []
 
-    def walk(self, startNode):
+    def walk(self, startNode, endNode, numCities):
         currentNode = startNode
+        # print(type(currentNode))
         currentEdge = None
-        while (not checkAllNodesPresent(self.visitedEdges)):
-            currentEdge = currentNode.rouletteWheel(self.visitedEdges, startNode)
+        while (not self.visitedEdges.__len__() >= numCities + 1):
+            # print(self.visitedEdges)
+            currentEdge = currentNode.rouletteWheel(self.visitedEdges, startNode, endNode, numCities)
+            # print(type(currentEdge))
             currentNode = currentEdge.toNode
             self.visitedEdges.append(currentEdge)
 
+        ''' 
+        #print(self.visitedEdges)
+      for edge in edges[currentNode.name.lower()]:
+            if(edge.toNode == endNode):
+                currentEdge = edge
+                self.visitedEdges.append(currentEdge)
+                print("duja")
+                break
+'''
+
     def pheromones(self):
+        global MAXCOST
         currentCost = getSum(self.visitedEdges)
         if (currentCost < MAXCOST):
             score = 1000 ** (1 - float(currentCost) / MAXCOST)  # Score function
             global bestScore
             global bestEdges
+            global currentScore
+            currentScore = score
             if (score > bestScore):
                 bestScore = score
                 bestEdges = self.visitedEdges
@@ -210,6 +239,42 @@ def checkAllEdges(edges):
         edge.checkPheromones()
 
 
+def runWalk(startCity, endCity, numCity=10, threshold=10):
+    global MAXCOST
+    last = 0
+    counter = 0
+    allSums = set()
+    for city in edges:
+        MAXCOST += getSum(edges[city])
+    while True:
+        if last == bestScore:
+            counter += 1
+            if counter == threshold:
+                # print("hola")
+                break
+        else:
+            # print("else")
+            counter = 0
+        for city in nodes:
+            evaporate(nodes[city].edges)
+            ant = ANT()
+            # print(type(nodes['aabjorgan']))
+            ant.walk(nodes[startCity], nodes[endCity], numCity)
+            ant.pheromones()
+            checkAllEdges(nodes[city].edges)
+            last = currentScore
+            # print i,getSum(ant.visitedEdges)
+            currentSum = getSum(ant.visitedEdges)
+            allSums.add(currentSum)
+    print(currentSum)
+    print(min(allSums))
+    # print(ant.visitedEdges)
+
+
+loader('no', 1000)
+runWalk('aabjorgan', 'aadalsbruk', 10, threshold=30)
+
+'''
 for i in range(100000):
     evaporate(edges)
     ant = ANT()
